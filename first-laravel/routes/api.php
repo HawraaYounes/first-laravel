32ec533f61b156010e36e536e9d4ddc444dd57c6
HawraaYounes <hawraayounes0@gmail.com>
2022-09-30 21:19:43 +0300

    PlaceNumberController:Adding placenumber functiom

--- first-laravel/routes/api.php
+++ first-laravel/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SortController;
-
+use App\Http\Controllers\PlaceNumberController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
