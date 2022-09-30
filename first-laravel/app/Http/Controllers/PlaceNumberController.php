d4d32810b9d3793a5976df7508c36c4f6219de73
HawraaYounes <hawraayounes0@gmail.com>
2022-09-30 23:52:41 +0300

    HumanToProgrammingController:Commenting the code

--- first-laravel/app/Http/Controllers/PlaceNumberController.php
+++ first-laravel/app/Http/Controllers/PlaceNumberController.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Request;
 class PlaceNumberController extends Controller
 {
     function placeNumber($num){
-        $arr=[];
-        $len=floor(log10($num));
+        $arr=[];//define empty array
+        $len=floor(log10($num));//define length of the number
         for ($i=0;$i<=$len+2;$i++){
-            $x=floor($num%10);
-            $x=$x*pow(10,$len);
-            $len--;
-            array_push($arr,$x);
-            $num=$num/10;
+            $x=floor($num%10);//round the number %10 to it's nearest value
+            $x=$x*pow(10,$len);//multiply the rounded result by 10 power length
+            array_push($arr,$x);//push x to the array
+            $len--;//decrement the length after x is pushed
+            $num=$num/10;//divide number by 10
         }
         return response()->json([
             "status"=>"success",
